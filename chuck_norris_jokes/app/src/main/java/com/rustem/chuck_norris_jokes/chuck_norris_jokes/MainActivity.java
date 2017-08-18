9b281c82aa43a4372bd136ec4a3db5c173992ea4
Sabitov Rustem <vkube2014@yandex.ru>
2017-08-18 23:56:03 +0300

    Шутки про Чака Норриса

---
+++ chuck_norris_jokes/app/src/main/java/com/rustem/chuck_norris_jokes/chuck_norris_jokes/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rustem.chuck_norris_jokes.chuck_norris_jokes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rustem.chuck_norris_jokes.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
